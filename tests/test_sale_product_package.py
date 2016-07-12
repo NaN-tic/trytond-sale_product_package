2ad9a4b49f2abb5ce8e73551d26b6e240b3c2d1d
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:50 +0200

    Update test to version 4.0

--- tests/test_sale_product_package.py
+++ tests/test_sale_product_package.py
@@ -4,29 +4,22 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_product_package')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_product_package')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_product_package'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_sale_product_package.rst',
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_sale_product_package.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
