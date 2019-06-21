b69ef0a4f360293be52eff4966d73badd77c031c
Raimon Esteve <raimon@nan-tic.com>
2019-06-21 14:32:35 +0200

    Not create a package when that already use the product without packages #038038

--- product.py
+++ product.py
@@ -11,6 +11,13 @@ class Package(metaclass=PoolMeta):
     __name__ = 'product.package'
 
     @classmethod
+    def __setup__(cls):
+        super(Package, cls).__setup__()
+        cls._create_package.append(
+            ('sale.line', 'sale_product_package.msg_product_package_null'),
+            )
+
+    @classmethod
     def find_packages(cls, records):
         find_packages = super(Package, cls).find_packages(records)
         if find_packages:
