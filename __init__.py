cd0a2450b894e859942d17ff3b0c1a878c468a5d
Raimon Esteve <raimon@nan-tic.com>
2019-06-21 12:55:21 +0200

    Not allow change quantity package when find packages in sale lines #038038

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from . import product
 from . import sale
 
 
 def register():
     Pool.register(
+        product.Package,
         sale.SaleLine,
         module='sale_product_package', type_='model')
     Pool.register(
